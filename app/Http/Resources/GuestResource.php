9b6ace3597df79b94f6447973dcbd92bbab426b8
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-11-14 14:19:26 +0700

    14/11/2025: Convert API P2

--- app/Http/Resources/GuestResource.php
+++ app/Http/Resources/GuestResource.php
@@ -10,12 +10,14 @@ class GuestResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'         => $this->id,
-            'full_name'  => $this->full_name,
-            'phone'      => $this->phone,
-            'status'     => (bool) $this->status,
-            'number'     => $this->number,
-            'qr_token'   => $this->qr_token,
+            'id' => $this->id,
+            'full_name' => $this->full_name,
+            'phone' => $this->phone,
+            'status' => (bool) $this->status,
+            'number' => $this->number,
+            'qr_token' => $this->qr_token,
+            'code_staft' => $this->code_staft,
+            'address' => $this->address,
             'invited_at' => $this->invited_at,
             'arrived_at' => $this->arrived_at,
 
