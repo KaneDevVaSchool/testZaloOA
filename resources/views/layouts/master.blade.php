910616dff088b14398ba70ae08b205d6709a92af
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-11-14 08:16:03 +0700

    s

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- SEO + Charset -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Zalo Verification -->
     <meta name="zalo-platform-site-verification" content="GUI20f7bBXzDbBOxYeaa65oOjqUmxcCPDZao" />
-    <title>Document</title>
+
+    <!-- Meta cơ bản -->
+    <title>Danh sách khách mời – Check-in sự kiện</title>
+    <meta name="description" content="Check-in khách mời và rút thăm may mắn tại sự kiện.">
+
+
+
 </head>
 <body>
     @yield('content')
