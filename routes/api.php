8ae43222c649431e6c7d05c20087c81dfe7fc62a
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-11-14 11:27:26 +0700

    d

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GuestController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('api/guests')->group(function () {
+    Route::get('/{qr_token}', [GuestController::class, 'showByQr'])->name('api.guests.show');
+    Route::get('/checkin/{qr_token}', [GuestController::class, 'checkInQRCode'])->name('api.guests.checkin');
+    Route::post('/scan', [GuestController::class, 'scanQr'])->name('api.guests.scan');
+    Route::get('/qr-image/{qr_token}', [GuestController::class, 'qrImage'])->name('api.guests.qr_image');
+    Route::get('/status/{qr_token}', [GuestController::class, 'status'])->name('api.guests.status');
+});
